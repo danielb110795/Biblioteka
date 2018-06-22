29e64a5c5a4500e179b8c746a889f2634c45da38
Piotr <barchanpiotr@gmail.com>
2018-06-22 20:21:49 +0200

    Działają wszystkie pierdoły oprócz czasu pracy pracownika.

--- src/controller/PracownikController.java
+++ src/controller/PracownikController.java
@@ -6,10 +6,13 @@ import java.util.List;
 import javax.ejb.EJB;
 import javax.ejb.LocalBean;
 import javax.ejb.Stateless;
+import javax.faces.context.FacesContext;
 import javax.inject.Named;
+import javax.servlet.http.HttpSession;
 
 import dao.PracownikDAO;
 import dao.UzytkownikDAO;
+import entity.Czytelnik;
 import entity.Pracownik;
 import entity.Uzytkownik;
 import lombok.Data;
@@ -64,5 +67,81 @@ public class PracownikController {
 		pracownicy = pracownikDAO.findAll();
 		
 		return pracownicy;
+	}
+	
+	public String edytujPracownika()
+	{
+		
+		
+		HttpSession session = (HttpSession) FacesContext.getCurrentInstance().getExternalContext().getSession(false);
+
+		String idPrac = (String) session.getAttribute("idPracownika");
+		
+		Long idPracownika = Long.parseLong(idPrac);
+		
+		
+		Pracownik pracownik = pracownikDAO.findOne(idPracownika);
+		Uzytkownik uzytkownik = uzytkownikDAO.findOne(pracownik.getUzytkownik().getId());
+		
+		if(login.equals(""))
+			uzytkownik.setLogin(uzytkownik.getLogin());
+		else
+			uzytkownik.setLogin(login);
+		if(haslo.equals(""))
+			uzytkownik.setHaslo(uzytkownik.getHaslo());
+		else
+			uzytkownik.setHaslo(haslo);
+		
+		uzytkownik.setRola("CZYTELNIK");
+		
+		uzytkownik.setAktywowane(true);
+		uzytkownik.setZalogowany(true);
+		
+		if(imie.equals(""))
+			pracownik.setImie(pracownik.getImie());
+		else
+			pracownik.setImie(imie);
+		if(nazwisko.equals(""))
+			pracownik.setNazwisko(pracownik.getNazwisko());
+		else
+			pracownik.setNazwisko(nazwisko);
+		
+		if(email.equals(""))
+			pracownik.setEmail(pracownik.getEmail());
+		else
+			pracownik.setEmail(email);
+		
+		if(pesel.equals(""))
+			pracownik.setPesel(pracownik.getPesel());
+		else	
+			pracownik.setPesel(pesel);
+		if(adres.equals(""))
+			pracownik.setAdres(pracownik.getAdres());
+		else
+			pracownik.setAdres(adres);
+		
+		pracownik.setBiblioteka(pracownik.getBiblioteka());
+		
+		pracownik.setUzytkownik(uzytkownik);
+		
+		uzytkownikDAO.save(uzytkownik);
+		pracownikDAO.save(pracownik); 
+		
+		return "pracownicy";
+	}
+	public String pokazEdycje(String id)
+	{
+	
+		HttpSession session = (HttpSession) FacesContext.getCurrentInstance().getExternalContext().getSession(false);
+		
+		session.setAttribute("idPracownika", id);
+		return "edytuj_pracownika.xhtml";
+	}
+	
+	public String usunPracownika(String id)
+	{
+		Long idPracownika = Long.parseLong(id);
+		pracownikDAO.remove(idPracownika);
+		return "pracownicy";
 	}
 }
